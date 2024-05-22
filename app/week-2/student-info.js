3dd6893e1c9e5b400dfb044ff930d19ddcda12d7
Komiboy1 <ebube624@gmail.com>
2024-05-21 22:52:45 -0600

    git add

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const StudentInfo = () => {
+    return (
+        <div>
+            <h1>Ebube Okonmah</h1>
+            <p>
+                <a href="https://github.com/Komiboy1/cprg306-assignments">https://github.com</a>
+            </p> 
+        </div>
+    )
+}
+
+export default StudentInfo;
