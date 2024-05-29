b0572b3e69a166831b142be2c31a9b00872b6329
Komiboy1 <ebubechukwu.okonmah@edu.sait.ca>
2024-05-28 22:14:01 -0600

    Updated /app/page.js

--- app/page.js
+++ app/page.js
@@ -1,14 +1,19 @@
 import Link from 'next/link';
 
-const Page = () => {
+export default function Page() {
   return(
-    <div>
+    <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p>
-        <a href='/week-2'>Week 2 Assignment</a>
-      </p>
-    </div>
+      <ul>
+        <li>
+          <Link href='week-2'>Week 2 Assignment</Link>
+        </li>
+        <li>
+          <Link href='week-3'>Week 3 Assignment</Link>
+        </li>
+        
+      </ul>
+    </main>
   )
 }
 
-export default Page;
