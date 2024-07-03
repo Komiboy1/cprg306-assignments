be9e185ab888f425525f708fc6498edf645cf829
Komiboy1 <ebubechukwu.okonmah@edu.sait.ca>
2024-07-02 23:25:21 -0600

    week 8 assignment

---
+++ app/week-8/_utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWazvcR4e_MKGtBoMkNPBB1sz_4hvZPwg",
+  authDomain: "cprg306-assignments-ef2c3.firebaseapp.com",
+  projectId: "cprg306-assignments-ef2c3",
+  storageBucket: "cprg306-assignments-ef2c3.appspot.com",
+  messagingSenderId: "784594891974",
+  appId: "1:784594891974:web:4f3cbbe515237265f753c7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
